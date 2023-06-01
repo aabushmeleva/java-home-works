86fe702245fc5d311afe925687caff7677a13ed5
Bushmeleva Anna <aabushmeleva@gmail.com>
2023-06-01 18:41:29 +0300

    [hw_2]Pair

--- hw2/pair/src/main/java/ru/qiwi/pair/Pair.java
+++ hw2/pair/src/main/java/ru/qiwi/pair/Pair.java
@@ -1,5 +1,30 @@
 package ru.qiwi.pair;
 
-public class Pair<One, Two> {
+public final class Pair<One, Two> {
+    private One first;
+    private Two second;
 
+    private Pair() {}
+    private Pair(One first, Two second) {
+        this.first = first;
+        this.second = second;
+    }
+    public static <One, Two>  Pair<One, Two> of(One first, Two second) {
+        Pair pair = new Pair(first, second);
+        return pair;
+    }
+
+    public One getFirst() {
+        return first;
+    }
+
+    public Two getSecond() {
+        return second;
+    }
+
+    public boolean equals(Pair obj) {
+        return (this.first == obj.first &&
+                this.second == obj.second);
+    }
 }
+
